7d110779dba1c05c399deb43714fcd8685b24142
johnharveymath <john.harvey.math@gmail.com>
2022-05-11 22:50:17 +0100

    Generating Figure 1 (B and C) using Python.

---
+++ manuscript-figures/generate_figure_1B.py
@@ -0,0 +1,40 @@
+import os
+import pandas as pd
+from plotnine import *
+
+data_path = os.path.join(os.getcwd(), "manuscript-figures/data")
+output_path = os.path.join(os.getcwd(), "manuscript-figures/output")
+
+# read in the GNI data
+file = os.path.join(data_path, "2020-09-15/gni_data.csv")
+x = pd.read_csv(file,
+                       header=0,
+                       usecols=['countrycode', 'gni_per_capita'])
+
+# read in the t_0 data
+file = os.path.join(data_path, "2021-09-15/figure_1b.csv")
+t0_df = pd.read_csv(file,
+                    delimiter=";",
+                    header=0,
+                    usecols=["countrycode", "days_to_t0_10_dead"],
+                    na_values=["N/A", "NA", "#N/A", " ", "", "None"]).rename(
+    columns={'days_to_t0_10_dead': 'days_to_t0'})
+
+# merge dataframes to plot
+z = pd.merge(x, t0_df, how="outer", on="countrycode").dropna()
+
+def number_formatter(array):
+    return ["{:,}".format(int(n)) for n in array]
+
+g = (ggplot(z, aes(x='gni_per_capita', y='days_to_t0'))
+     + geom_point(shape = 1)
+     + geom_smooth(method = "lm", colour = "#7a0177", fill = "#c51b8a")
+     + scale_x_log10(labels=number_formatter)
+     + scale_y_log10()
+     + labs(x="GNI per capita", y="Days until epidemic established")
+     + theme_bw()
+     + theme(axis_title = element_text(face = "bold"))
+     )
+
+file = os.path.join(output_path, "1B.png")
+g.save(filename=file, height=7.4, width=14.7, units="cm", dpi=500)
